9db2e6e47a0e52f8cce736c6c92588d5dfebbb6f
s851218 <sin0981802858@gmail.com>
2024-08-24 16:19:04 +0800

    首頁 update

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log("Hello world!");
+// 首頁/精選文章/Swiper
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  loop: true,
+  spaceBetween: 48,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination-custom",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
